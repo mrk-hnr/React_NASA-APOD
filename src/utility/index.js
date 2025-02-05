667085045594037e53b82fc5cfd4cf2d49bb3db7
mrk-hnr <mrk.hnr@hotmail.com>
2025-02-05 12:00:47 +0800

    getLast7Days changed current date to prevdate

--- src/utility/index.js
+++ src/utility/index.js
@@ -6,25 +6,27 @@ export const APODWeek = [
 ] 
 
 
-export const getLast7Days = () => {
-    const dates = [];
-    for (let i = 0; i < 7; i++) {
-      const date = new Date();
-      date.setDate(date.getDate() - i);
-      
-      // dates.push(date.toISOString().split('T')[0]); // Format the date as YYYY-MM-DD
-
-      // dates.push(`${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`); // Display the date as MM-DD-YYYY but without the zero in front of MM and DD
-
-      // MM-DD-YYYY with zero in front of MM and DD when below 10 days
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-      const year = date.getFullYear();
-      dates.push(`${month}-${day}-${year}`);
-    
-    }
-    return dates
-    console.log(dates[0])
-  };
 
   
+
+  export const getLast7Days = () => {
+    const dates = [];
+    for (let i = 1; i < 7; i++) { // Since Asia is ahead in terms of time, For loop starts at one to use prev day as current date to prevent error for residence ahead of US. 
+        const date = new Date();
+        date.setDate(date.getDate() - i);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        dates.push(`${year}-${month}-${day}`);
+
+        // dates.push(`${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`);
+        // Display the date as MM-DD-YYYY but without the zero in front of MM and DD
+
+         // MM-DD-YYYY with zero in front of MM and DD when below 10 days
+      // const month = String(date.getMonth() + 1).padStart(2, '0');
+      // const day = String(date.getDate()).padStart(2, '0');
+      // const year = date.getFullYear();
+      // dates.push(`${month}-${day}-${year}`);
+    }
+    return dates;
+};
